dcb099dbba9e9404cf0de25029363ecccc4ffec8
Hrish-1 <hrishikeshm997@gmail.com>
2023-05-21 01:17:42 +0530

    user resource scaffolding

---
+++ src/main/java/com/example/testcontainers/users/User.java
@@ -0,0 +1,12 @@
+package com.example.testcontainers.users;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public record User(
+    @Id String id,
+    String name,
+    Integer age
+) {
+}
---
+++ src/main/java/com/example/testcontainers/users/api/v1/UserController.java
@@ -0,0 +1,41 @@
+package com.example.testcontainers.users.api.v1;
+
+import java.util.List;
+
+import com.example.testcontainers.users.User;
+import com.example.testcontainers.users.UserRepository;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping("/v1/users")
+@AllArgsConstructor
+public class UserController {
+    UserRepository repo;
+
+    @GetMapping
+    public List<User> list() {
+       return repo.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void create(@RequestBody List<User> user) {
+        repo.saveAll(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable String id) {
+        repo.deleteById(id);
+    }
+}
